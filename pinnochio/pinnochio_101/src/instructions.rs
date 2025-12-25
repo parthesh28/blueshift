e35cd81b78c4c3d74e4ffa94b19b2ea1dc2200d9
Parthesh28 <partheshpurohit28@gmail.com>
2025-12-25 17:45:17 +0530

    completed pinocchio vault

---
+++ pinnochio/pinnochio_101/src/instructions.rs
@@ -0,0 +1,123 @@
+use core::convert::TryFrom;
+use core::mem::size_of;
+use pinocchio::{
+    ProgramResult,
+    account_info::AccountInfo,
+    program_error::ProgramError,
+    pubkey::{Pubkey, find_program_address},
+    sysvars::{Sysvar, rent::Rent},
+};
+use pinocchio_log::log;
+use pinocchio_system::instructions::{CreateAccount, Transfer as SystemTransfer};
+
+pub struct Deposit<'a> {
+    pub owner: &'a AccountInfo,
+    pub vault: &'a AccountInfo,
+    pub amount: u64,
+}
+
+fn parse_amount(data: &[u8]) -> Result<u64, ProgramError> {
+    if data.len() != core::mem::size_of::<u64>() {
+        return Err(ProgramError::InvalidInstructionData);
+    }
+    let amount = u64::from_le_bytes(data.try_into().unwrap());
+
+    if amount == 0 {
+        return Err(ProgramError::InvalidInstructionData);
+    }
+
+    Ok(amount)
+}
+
+fn derive_vault(owner: &AccountInfo) -> (Pubkey, u8) {
+    find_program_address(&[b"vault", owner.key().as_ref()], &crate::ID)
+}
+
+fn check_vault(owner: &AccountInfo, vault: &AccountInfo) -> ProgramResult {
+    if !owner.is_signer() {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    if vault.data_is_empty() {
+        const ACCOUNT_DISCRIMINATOR_SIZE: usize = 8;
+
+       let (expected_vault, _bump) = derive_vault(owner);
+
+        // ensure the provided vault is same as the derived
+        if vault.key() != &expected_vault {
+            return Err(ProgramError::InvalidSeeds);
+        }
+
+        const VAULT_SIZE: usize = ACCOUNT_DISCRIMINATOR_SIZE + size_of::<u64>();
+        let needed_lamports = Rent::get()?.minimum_balance(VAULT_SIZE);
+
+        CreateAccount {
+            from: owner,
+            to: vault,
+            lamports: needed_lamports,
+            space: VAULT_SIZE as u64,
+            owner: &crate::ID,
+        }
+        .invoke()?;
+
+        log!("Vault created");
+    } else {
+        if !vault.is_owned_by(&crate::ID) {
+            return Err(ProgramError::InvalidAccountOwner);
+        }
+
+        log!("Vault already exists");
+    }
+
+    Ok(())
+}
+
+impl<'a> Deposit<'a> {
+    pub const DISCRIMINATOR: u8 = 0;
+
+    pub fn process(self) -> ProgramResult {
+        let Deposit {
+            owner,
+            vault,
+            amount,
+        } = self;
+
+        check_vault(owner, vault)?;
+
+        SystemTransfer {
+            from: owner,
+            to: vault,
+            lamports: amount,
+        }
+        .invoke()?;
+
+        log!("{} Lamports deposited to vault", amount);
+        Ok(())
+    }
+}
+
+impl<'a> TryFrom<(&'a [u8], &'a [AccountInfo])> for Deposit<'a> {
+    type Error = ProgramError;
+
+    fn try_from(value: (&'a [u8], &'a [AccountInfo])) -> Result<Self, Self::Error> {
+        let (data, accounts) = value;
+        if accounts.len() < 3 {
+            return Err(ProgramError::NotEnoughAccountKeys);
+        }
+        let owner = &accounts[0];
+        let vault = &accounts[1];
+        let system_program = &accounts[2];
+        let amount = parse_amount(data)?;
+
+        if system_program.key() != &pinocchio_system::ID {
+            return Err(ProgramError::IncorrectProgramId);
+        }
+
+
+        Ok(Self {
+            owner,
+            vault,
+            amount,
+        })
+    }
+}
---
+++ pinnochio/pinnochio_101/src/main.rs
@@ -0,0 +1,30 @@
+// lib.rs
+#![no_std]
+
+use pinocchio::{
+    account_info::AccountInfo,
+    entrypoint,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    ProgramResult,
+};
+use pinocchio_pubkey::declare_id;
+
+entrypoint!(process_instruction);
+
+pub mod instructions;
+pub use instructions::*;
+
+declare_id!("G13onQ4TcR48UiLXHou9zyDT7wq5gH7oKMrWRmo2NJDR");
+
+fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    match instruction_data.split_first() {
+        Some((Deposit::DISCRIMINATOR, data)) => Deposit::try_from((data, accounts))?.process(),
+        Some((Withdraw::DISCRIMINATOR, _)) => Withdraw::try_from(accounts)?.process(),
+        _ => Err(ProgramError::InvalidInstructionData),
+    }
+}
